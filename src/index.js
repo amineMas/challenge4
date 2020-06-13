c632c6ab925d5018826c3905eee4ef03dc3b60f3
Amine <aminemastouri@gmail.com>
2020-06-13 12:32:11 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import ScotchInfoBar from "./ScotchInfoBar";
+import "./styles.css";
+
+function App() {
+  const [passPhrase, setPassPhrase] = useState("");
+
+  if (passPhrase === "open sesame") {
+    alert("You may pass");
+  }
+
+  return (
+    <div className="App">
+      <h2>What's the secret phrase?</h2>
+
+      <input
+        type="text"
+        placeholder="Super duper secret"
+        value={passPhrase}
+        onChange={e => setPassPhrase(e.target.value)}
+      />
+
+      <p>
+        Hint: It's <strong>open sesame</strong>
+      </p>
+
+      <ScotchInfoBar seriesNumber={4} />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
